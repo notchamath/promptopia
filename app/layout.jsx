f27f6bcae0f5c42a77a857794312c752eb4ef110
Chamath Jayarathne <chamathsocial@gmail.com>
2024-06-23 19:25:42 -0400

    NextJS project setup

---
+++ app/layout.jsx
@@ -0,0 +1,25 @@
+import '@styles/globals.css';
+import { Children } from 'react';
+
+export const metadata = {
+  title: "Promptopia",
+  description: 'Discover and share AI prompts.'
+}
+
+const RootLayout = ({children}) => {
+  return (
+    <html lang='en'>
+      <body>
+        <div className="main">
+          <div className="gradient"/>
+        </div>
+
+        <main className="app">
+          {children}
+        </main>
+      </body>
+    </html>
+  )
+}
+
+export default RootLayout;
